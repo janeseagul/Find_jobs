577edb37633ef537804eff9b611d1f717c8ee729
Marta Chaika <janeseagul@gmail.com>
2023-03-09 13:29:37 +0300

    Исправила код.
    Исправила названия переменных
    Починила функцию получения статистики для SuperJob
    Убрала слова-паразиты из названий функций

--- main.py
+++ main.py
@@ -2,26 +2,19 @@ import requests
 from terminaltables import AsciiTable
 import os
 
-def predict_rub_salary(vacancy):
-    try:
-        salary = vacancy['salary']
-        if salary['currency'] != 'RUR':
-            return None
-        if salary['from'] and salary['to']:
-            return (salary['from'] + salary['to']) // 2
-        elif salary['from']:
-            return salary['from'] * 1.2
-        elif salary['to']:
-            return salary['to'] * 0.8
-        else:
-            return None
-    except TypeError:
-        return None
+
+def predict_rub_salary(salary_from, salary_to=None):
+    if salary_from and salary_to:
+        return (salary_from + salary_to) // 2
+    if salary_from:
+        return int(salary_from * 1.2)
+    if salary_to:
+        return int(salary_to * 0.8)
 
 
-def get_vacancies_dataset_hh(languages):
+def get_vacancies_stats_hh(languages):
     url = 'https://api.hh.ru/vacancies'
-    vacancies_data = {}
+    vacancies_stats = {}
     for language in languages:
         page = 0
         salaries_count = 0
@@ -32,27 +25,27 @@ def get_vacancies_dataset_hh(languages):
                 'area': 1,
                 'per_page': 100,
                 'page': page,
-                'only_with_salary': True
             }
             response = requests.get(url, params=params)
+            response.raise_for_status()
             data = response.json()
             if not data['items']:
                 break
             for vacancy in data['items']:
-                salary = predict_rub_salary(vacancy)
+                salary = predict_rub_salary(vacancy.get('salary', {}).get('from'), vacancy.get('salary', {}).get('to'))
                 if salary:
                     salaries_count += 1
                     salaries_sum += salary
             page += 1
         if salaries_count:
-           vacancies_data[language] = {
+            vacancies_stats[language] = {
                 'vacancies_found': data['found'],
-                'vacancies_processed': page * 100,
+                'vacancies_processed': params['page'] * 100,
                 'salaries_found': salaries_count,
-                'average_salary': int(salaries_sum / salaries_count)
+                'average_salary': int(salaries_sum / salaries_count) if salaries_count else 0
             }
 
-    return vacancies_data
+    return vacancies_stats
 
 
 def predict_rub_salary_hh(vacancy):
@@ -76,44 +69,45 @@ def predict_rub_salary_hh(vacancy):
     return predict_rub_salary(salary_from, salary_to)
 
 
-def get_vacancies_dataset_sj(languages, sj_api_key):
+def get_vacancies_stats_sj(languages, sj_api_key):
     url = 'https://api.superjob.ru/2.0/vacancies/'
     headers = {
         'X-Api-App-Id': sj_api_key,
     }
-    vacancies_data = {}
+    vacancies_stats = {}
     for language in languages:
         page = 0
         salaries_count = 0
         salaries_sum = 0
         while True:
             params = {
-                'page': page,
+                'keyword': f'программист {language}',
+                'town': 'Москва',
                 'count': 100,
-                'town': 4,
-                'period': 30,
-                'keyword': language,
-                'keywords': ['программист', 'программиста', 'разработка']
+                'page': page
             }
             response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
             data = response.json()
-            if not data['items']:
+            if not data['objects']:
                 break
-            for vacancy in data['items']:
-                salary = predict_rub_salary(vacancy)
+            for vacancy in data['objects']:
+                salary = predict_rub_salary(vacancy.get('payment', {}).get('from'),
+                                            vacancy.get('payment', {}).get('to'))
                 if salary:
                     salaries_count += 1
                     salaries_sum += salary
             page += 1
         if salaries_count:
-             vacancies_data({
-                 'languages':language,
-                 'vacancies_found': data['found'],
-                 'vacancies_processed': page * 100,
-                 'average_salary': int(salaries_sum / salaries_count)
-            })
+            vacancies_stats[language] = {
+                'vacancies_found': data['more'],
+                'vacancies_processed': params['page'] * 100,
+                'salaries_found': salaries_count,
+                'average_salary': int(salaries_sum / salaries_count) if salaries_count else 0
+            }
 
-    return vacancies_data
+    return vacancies_stats
+
 
 def predict_rub_salary_sj(vacancy):
     if not vacancy['currency'] == 'rub':
@@ -123,18 +117,18 @@ def predict_rub_salary_sj(vacancy):
     return predict_rub_salary(salary_from, salary_to)
 
 
-def draw_table(dataset, title):
+def draw_table(stats, title):
     table_lines = [
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано',
          'Средняя зарплата']
     ]
-    for language in dataset:
+    for language in stats:
         table_lines.append(
-            [   
+            [
                 language,
-                dataset[language]["vacancies_found"],
-                dataset[language]["vacancies_processed"],
-                dataset[language]["average_salary"],
+                stats[language]["vacancies_found"],
+                stats[language]["vacancies_processed"],
+                stats[language]["average_salary"],
             ]
         )
     table_instance = AsciiTable(table_lines, title)
@@ -160,13 +154,13 @@ def main():
         'TypeScript'
     ]
     sj_api_key = os.environ['SJ_API_KEY']
-    dataset_hh = get_vacancies_dataset_hh(languages)
+    stats_hh = get_vacancies_stats_hh(languages)
     title_hh = 'HeadHunter Moscow'
-    draw_table(dataset_hh, title_hh)
-    print()
-    dataset_sj = get_vacancies_dataset_sj(languages, sj_api_key)
+    draw_table(stats_hh, title_hh)
+    stats_sj = get_vacancies_stats_sj(languages, sj_api_key)
     title_sj = 'SuperJob Moscow'
-    draw_table(dataset_sj, title_sj)
+    draw_table(stats_sj, title_sj)
+
 
 if __name__ == '__main__':
     main()
